a369189b045716a6478fc85d00f2cf0f4c9ff059
shinyaOsawa <>
2019-08-08 13:43:55 +0900

    edit.blade.phpのフォームタグの記述を、laravel collectiveにて統一。

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -5,19 +5,22 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => ['report.update',  $report->id], 'method' => 'PUT']) !!}
-      <input class="form-control" name="user_id" type="hidden" value="{{ $report->user_id }}">
+      <!-- <input class="form-control" name="user_id" type="hidden" value="{{ $report->user_id }}"> -->
       <div class="form-group form-size-small">
-        <input class="form-control" name="reporting_time" type="date" value="{{ $report->reporting_time }}">
-      <span class="help-block"></span>
+        <!-- <input class="form-control" name="reporting_time" type="date" value="{{ $report->reporting_time->format('Y-m-d') }}"> -->
+        {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
+        <span class="help-block"></span>
       </div>
       <div class="form-group @if($errors->has('title')) has-error @endif">
-        <input class="form-control" placeholder="Title" name="title" type="text" value="{{ $report->title }}">
+        <!-- <input class="form-control" placeholder="Title" name="title" type="text" value="{{ $report->title }}"> -->
+        {!! Form::input('text', 'title', $report->title, ['class' => 'form-control']) !!}
       <span class="help-block">
         {{ $errors->first('title') }}
       </span>
       </div>
       <div class="form-group @if($errors->has('contents')) has-error @endif">
-        <textarea class="form-control" placeholder="本文" name="contents" cols="50" rows="10">{{ $report->contents }}</textarea>
+        <!-- <textarea class="form-control" placeholder="本文" name="contents" cols="50" rows="10">{{ $report->contents }}</textarea> -->
+        {!! Form::textarea('contents', $report->contents, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
       <span class="help-block">
         {{ $errors->first('contents') }}
       </span>
